ada270bbd0a7c32a6412606e5749132b1bfa6440
José Manuel <81641564+jose32coder@users.noreply.github.com>
2022-02-10 17:56:12 -0400

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -44,7 +44,6 @@ const colorPalette = document.querySelectorAll('.choose-color span');
 var root = document.querySelector(':root');
 const bg1= document.querySelector('.bg-1');
 const bg2= document.querySelector('.bg-2');
-const bg3= document.querySelector('.bg-3');
 
 
 const openThemeModal = () => {
@@ -95,7 +94,7 @@ colorPalette.forEach(color => {
         let primaryHue;
         changeActiveColorClass();
         if(color.classList.contains('color-1')){
-            primaryHue = 152;
+            primaryHue = 160;
         }
         else if(color.classList.contains('color-2')){
             primaryHue = 315;
@@ -128,7 +127,6 @@ const changeBG = () =>{
 bg1.addEventListener('click', () =>{
     bg1.classList.add('active');
     bg2.classList.remove('active');
-    bg3.classList.remove('active');
     window.location.reload()
 })
 
@@ -139,20 +137,9 @@ bg2.addEventListener('click', () =>{
 
     bg2.classList.add('active');
     bg1.classList.remove('active');
-    bg3.classList.remove('active');
     changeBG();
 })
 
-bg3.addEventListener('click', () =>{
-    darkColorLightness = '95%';
-    whiteColorLightness = '10%';
-    lightColorLightness = '0%';
-
-    bg3.classList.add('active');
-    bg2.classList.remove('active');
-    bg1.classList.remove('active');
-    changeBG();
-})
 
 /* ================ AUDIO ================== */
 
@@ -176,3 +163,72 @@ icon2.onclick = sonar;
         } 
     }
     
+
+    
+/* ================ Memory Card ================== */
+
+
+const cards = document.querySelectorAll('.cardi');
+
+let matched = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+
+function flipCard({target: clickedCard}) {
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matched++;
+        if(matched == 8) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 5000);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+
+function shuffleCard() {
+    matched = 0;
+    disableDeck = false;
+    cardOne = cardTwo = "";
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    cards.forEach((card, i) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector(".back-view img");
+        imgTag.src = `assets/img/tecnologias/img-${arr[i]}.png`;
+        card.addEventListener("click", flipCard);
+    });
+}
+
+shuffleCard();
+    
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
